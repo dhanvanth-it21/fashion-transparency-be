79f429408ecc1a34f19ce10f24c4a0d826f1eb49
Dhanvanth Sathyamangalam Balakrishnan <dhanvanth.balakrishnan@int.trustrace.com>
2025-02-22 07:52:31 +0530

    be init

---
+++ src/main/java/com/trustrace/tiles_hub_be/TilesHubBeApplication.java
@@ -0,0 +1,13 @@
+package com.trustrace.tiles_hub_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilesHubBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilesHubBeApplication.class, args);
+	}
+
+}
